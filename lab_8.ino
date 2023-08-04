b54787141d10ea323dd61d3423e1e25aad734ab0
rony <ronyramos495@gmail.com>
2023-08-04 12:07:45 -0500

    se iniclaizo la carpeta con los certificados y el codigo inicial.

---
+++ lab_8.ino
@@ -0,0 +1,14 @@
+#include "FS.h"
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+
+void setup() {
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
